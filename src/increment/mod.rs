8d19ed124c3a00fc7742aa49939374462dd83394
C <DeveloperC@protonmail.com>
2020-05-14 01:49:21 +0000

    refactor: moving regex into is functions

--- src/increment/mod.rs
+++ src/increment/mod.rs
@@ -13,33 +13,20 @@ fn get_next_version_from_commits_consecutive(
     commit_messages: Vec<String>,
     mut semantic_version: Version,
 ) -> Version {
-    lazy_static! {
-        static ref MAJOR_TITLE_INCREMENT_REGEX: Regex =
-            Regex::new(r"(?i)^([[:word:]]*)(!(\([[:word:]]*\))?|(\([[:word:]]*\))?!):").unwrap();
-        static ref MAJOR_FOOTER_INCREMENT_REGEX: Regex =
-            Regex::new(r"(?i)^([[:word:]]*)(\([[:word:]]*\))?:(.)*(\n)*BREAKING CHANGE:").unwrap();
-        static ref MINOR_INCREMENT_REGEX: Regex =
-            Regex::new(r"(?i)^feat(\([[:word:]]*\))?:").unwrap();
-        static ref PATCH_INCREMENT_REGEX: Regex =
-            Regex::new(r"(?i)^fix(\([[:word:]]*\))?:").unwrap();
-    }
-
     for (_i, commit_message) in commit_messages.iter().enumerate() {
-        if MAJOR_TITLE_INCREMENT_REGEX.is_match(commit_message)
-            || MAJOR_FOOTER_INCREMENT_REGEX.is_match(commit_message)
-        {
+        if is_major_increment(commit_message) {
             trace!(
                 "Incrementing major version because of commit {:?}.",
                 commit_message
             );
             semantic_version.increment_major();
-        } else if MINOR_INCREMENT_REGEX.is_match(commit_message) {
+        } else if is_minor_increment(commit_message) {
             trace!(
                 "Incrementing minor version because of commit {:?}.",
                 commit_message
             );
             semantic_version.increment_minor();
-        } else if PATCH_INCREMENT_REGEX.is_match(commit_message) {
+        } else if is_patch_increment(commit_message) {
             trace!(
                 "Incrementing patch version because of commit {:?}.",
                 commit_message
@@ -49,6 +36,36 @@ fn get_next_version_from_commits_consecutive(
     }
 
     return semantic_version;
+}
+
+fn is_major_increment(commit_message: &str) -> bool {
+    lazy_static! {
+        static ref MAJOR_TITLE_INCREMENT_REGEX: Regex =
+            Regex::new(r"(?i)^([[:word:]]*)(!(\([[:word:]]*\))?|(\([[:word:]]*\))?!):").unwrap();
+        static ref MAJOR_FOOTER_INCREMENT_REGEX: Regex =
+            Regex::new(r"(?i)^([[:word:]]*)(\([[:word:]]*\))?:(.)*(\n)*BREAKING CHANGE:").unwrap();
+    }
+
+    return MAJOR_TITLE_INCREMENT_REGEX.is_match(commit_message)
+        || MAJOR_FOOTER_INCREMENT_REGEX.is_match(commit_message);
+}
+
+fn is_minor_increment(commit_message: &str) -> bool {
+    lazy_static! {
+        static ref MINOR_INCREMENT_REGEX: Regex =
+            Regex::new(r"(?i)^feat(\([[:word:]]*\))?:").unwrap();
+    }
+
+    return MINOR_INCREMENT_REGEX.is_match(commit_message);
+}
+
+fn is_patch_increment(commit_message: &str) -> bool {
+    lazy_static! {
+        static ref PATCH_INCREMENT_REGEX: Regex =
+            Regex::new(r"(?i)^fix(\([[:word:]]*\))?:").unwrap();
+    }
+
+    return PATCH_INCREMENT_REGEX.is_match(commit_message);
 }
 
 #[cfg(test)]
--- src/increment/tests.rs
+++ src/increment/tests.rs
@@ -3,66 +3,47 @@ use super::*;
 use rstest::rstest;
 
 #[rstest(
-    commit_messages,
-    semantic_version,
-    expected_next_version,
-    case(vec!["fix(default): Remove undocumented alias of default() (#469)\n\nBREAKING CHANGE:\r\n\r\nremoved undocumented `defaults` alias for `default`.\r\n\r\n".to_string()], "1.2.4", "2.0.0"),
-    case(vec!["feat: apply default builder to command() and apply fail() handlers globally (#583)\n\nBREAKING CHANGE: fail is now applied globally.\r\nBREAKING CHANGE: we now default to an empty builder function when command is executed with no builder.".to_string()], "13.1.22", "14.0.0"),
+    commit_message,
+    expected,
+    case("fix(default): Remove undocumented alias of default() (#469)\n\nBREAKING CHANGE:\r\n\r\nremoved undocumented `defaults` alias for `default`.\r\n\r\n", true),
+    case("feat: apply default builder to command() and apply fail() handlers globally (#583)\n\nBREAKING CHANGE: fail is now applied globally.\r\nBREAKING CHANGE: we now default to an empty builder function when command is executed with no builder.", true),
+    case("refactor!: drop support for Node 6", true),
+    case("feat(deps)!: pull in yargs-parser@17.0.0 (#1553)\n\n", true),
+    case("build!(deps): updating build deps to latest versions", true),
 )]
-fn test_get_next_version_from_commits_consecutive_footer_major_increment(
-    commit_messages: Vec<String>,
-    semantic_version: &str,
-    expected_next_version: &str,
-) {
-    assert_expected_returned(commit_messages, semantic_version, expected_next_version);
+fn test_is_major_increment(commit_message: &str, expected: bool) {
+    assert_eq!(expected, is_major_increment(commit_message));
 }
 
 #[rstest(
-    commit_messages,
-    semantic_version,
-    expected_next_version,
-    case(vec!["refactor!: drop support for Node 6".to_string()], "0.2.4", "1.0.0"),
-    case(vec!["feat(deps)!: pull in yargs-parser@17.0.0 (#1553)\n\n".to_string()], "2.1.10", "3.0.0"),
-    case(vec!["build!(deps): updating build deps to latest versions".to_string()], "2.1.10", "3.0.0"),
+    commit_message,
+    expected,
+    case("feat: add usage for single-digit boolean aliases (#1580)\n\n", true),
+    case("FEAT: add usage for single-digit boolean aliases (#1580)\n\n", true),
+    case("feat(completion): takes negated flags into account when", true)
 )]
-fn test_get_next_version_from_commits_consecutive_major_increment(
-    commit_messages: Vec<String>,
-    semantic_version: &str,
-    expected_next_version: &str,
-) {
-    assert_expected_returned(commit_messages, semantic_version, expected_next_version);
+fn test_is_minor_increment(commit_message: &str, expected: bool) {
+    assert_eq!(expected, is_minor_increment(commit_message));
 }
 
 #[rstest(
-    commit_messages,
-    semantic_version,
-    expected_next_version,
-    case(vec!["feat: add usage for single-digit boolean aliases (#1580)\n\n".to_string()], "1.0.0", "1.1.0"),
-    case(vec!["FEAT: add usage for single-digit boolean aliases (#1580)\n\n".to_string()], "1.0.0", "1.1.0"),
-    case(vec!["feat(completion): takes negated flags into account when".to_string()], "2.2.4", "2.3.0"),
+    commit_message,
+    expected,
+    case(
+        "fix: __proto__ will now be replaced with ___proto___ in parse (#1591)",
+        true
+    ),
+    case(
+        "FIX: __proto__ will now be replaced with ___proto___ in parse (#1591)",
+        true
+    ),
+    case(
+        "fix(deps): fix enumeration for normalized path arguments (#1567)\n\n",
+        true
+    )
 )]
-fn test_get_next_version_from_commits_consecutive_minor_increment(
-    commit_messages: Vec<String>,
-    semantic_version: &str,
-    expected_next_version: &str,
-) {
-    assert_expected_returned(commit_messages, semantic_version, expected_next_version);
-}
-
-#[rstest(
-    commit_messages,
-    semantic_version,
-    expected_next_version,
-    case(vec!["fix: __proto__ will now be replaced with ___proto___ in parse (#1591)".to_string()], "0.3.2", "0.3.3"),
-    case(vec!["FIX: __proto__ will now be replaced with ___proto___ in parse (#1591)".to_string()], "1.1.0", "1.1.1"),
-    case(vec!["fix(deps): fix enumeration for normalized path arguments (#1567)\n\n".to_string()], "2.1.2", "2.1.3"),
-)]
-fn test_get_next_version_from_commits_consecutive_patch_increment(
-    commit_messages: Vec<String>,
-    semantic_version: &str,
-    expected_next_version: &str,
-) {
-    assert_expected_returned(commit_messages, semantic_version, expected_next_version);
+fn test_is_patch_increment(commit_message: &str, expected: bool) {
+    assert_eq!(expected, is_patch_increment(commit_message));
 }
 
 #[rstest(
@@ -79,18 +60,11 @@ fn test_get_next_version_from_commits_consecutive_consecutive_increments(
     semantic_version: &str,
     expected_next_version: &str,
 ) {
-    assert_expected_returned(commit_messages, semantic_version, expected_next_version);
-}
-
-fn assert_expected_returned(
-    commit_messages: Vec<String>,
-    semantic_version: &str,
-    expected_next_version: &str,
-) {
     //when
-    let returned_next_version = get_next_version_from_commits_consecutive(
+    let returned_next_version = get_next_version_from_commits(
         commit_messages,
         Version::parse(semantic_version).unwrap(),
+        false,
     )
     .to_string();
 
