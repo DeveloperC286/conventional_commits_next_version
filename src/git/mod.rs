521a70edbfbabbd1833731ac5b6c8959c25b6c7d
C <DeveloperC@protonmail.com>
2020-05-25 18:41:41 +0000

    refactor: added additional git error handling

--- src/git/mod.rs
+++ src/git/mod.rs
@@ -2,29 +2,24 @@ pub fn get_commit_messages_from(from_commit_hash: &str) -> Vec<String> {
     let mut commit_messages = vec![];
 
     let repository = get_local_repository();
-    let mut walker = repository.revwalk().unwrap();
-    walker.push_head().unwrap();
-    walker.hide(get_oid(from_commit_hash)).unwrap();
+    let mut revwalk = get_revwalk(&repository, get_oid(from_commit_hash));
 
     loop {
-        match walker.next() {
-            Some(Ok(oid)) => {
-                let commit_message_optional = get_commit_message(&repository, oid);
-                match commit_message_optional {
-                    Some(commit_message) => {
-                        trace!(
-                            "For the commit id '{}' found the message {:?}.",
-                            oid,
-                            commit_message
-                        );
-                        commit_messages.push(commit_message);
-                    }
-                    None => {
-                        warn!("Commit with the id '{}' has no commit message.", oid);
-                    }
+        match revwalk.next() {
+            Some(Ok(oid)) => match get_commit_message(&repository, oid) {
+                Some(commit_message) => {
+                    trace!("Adding commit '{}'s message {:?}.", oid, commit_message);
+                    commit_messages.push(commit_message);
                 }
+                None => {
+                    warn!("Commit '{}' has no message.", oid);
+                }
+            },
+            Some(Err(error)) => {
+                error!("Revwalk threw error while walking the commits.");
+                error!("{:?}", error);
+                std::process::exit(1);
             }
-            Some(Err(error)) => error!("{:?}", error),
             None => break,
         }
     }
@@ -33,16 +28,42 @@ pub fn get_commit_messages_from(from_commit_hash: &str) -> Vec<String> {
     return commit_messages;
 }
 
+fn get_revwalk(repository: &git2::Repository, from_commit_hash: git2::Oid) -> git2::Revwalk {
+    match repository.revwalk() {
+        Ok(mut revwalk) => {
+            match revwalk.push_head() {
+                Ok(_result) => {}
+                Err(_error) => {
+                    error!("Unable to push HEAD onto the revwalk.");
+                    std::process::exit(1);
+                }
+            }
+
+            match revwalk.hide(from_commit_hash) {
+                Ok(_result) => {}
+                Err(_error) => {
+                    error!("Can not find commit '{}' on the revwalk.", from_commit_hash);
+                    std::process::exit(1);
+                }
+            }
+
+            return revwalk;
+        }
+        Err(error) => {
+            error!("Unable to get revwalk from local repository.");
+            error!("{:?}", error);
+            std::process::exit(1);
+        }
+    };
+}
+
 fn get_commit_message(repository: &git2::Repository, oid: git2::Oid) -> Option<String> {
     match repository.find_commit(oid) {
         Ok(commit) => {
             return commit.message().map(|m| m.to_string());
         }
         Err(_error) => {
-            error!(
-                "Can not find commit with id '{}' in current repository.",
-                oid
-            );
+            error!("Can not find commit '{}' in current repository.", oid);
             std::process::exit(1);
         }
     }
@@ -52,7 +73,7 @@ fn get_local_repository() -> git2::Repository {
     match git2::Repository::open(std::path::PathBuf::from(".")) {
         Ok(repository) => return repository,
         Err(_error) => {
-            error!("Not in a Git repository.");
+            error!("Current directory is not a Git repository.");
             std::process::exit(1);
         }
     }
