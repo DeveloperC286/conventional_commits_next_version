74ecaf2e0f1b30b95a6f1c401918ba3934a6d3d4
C <DeveloperC@protonmail.com>
2020-09-20 00:17:11 +0100

    refactor: removing superfluous Git environment scenarios

--- end-to-end-tests/features/steps/git.py
+++ end-to-end-tests/features/steps/git.py
@@ -24,7 +24,7 @@ def clone_remote_repository_and_checkout_commit(context, remote_repository, comm
     os.chdir(current_directory)
 
 
-@given('the GIT_DIR environment variable is set to the cloned repository and the working directory is changed.')
+@given('the GIT_DIR environment variable is set to the cloned repository.')
 def set_git_dir(context):
     os.environ["GIT_DIR"] = str(context.temporary_directory.name + "/.git")
 
