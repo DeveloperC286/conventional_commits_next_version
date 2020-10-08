8db6dbf9a39c52aebad8b4d47ff14385c541d252
C <DeveloperC@protonmail.com>
2020-10-08 19:23:47 +0000

    test: patch empty scope tests

--- src/increment/tests/patch/mod.rs
+++ src/increment/tests/patch/mod.rs
@@ -56,3 +56,22 @@ fn test_is_patch_increment_preceding_whitespace(commit_message: &str) {
 fn test_is_not_patch_increment_preceding_whitespace(commit_message: &str) {
     assert_eq!(false, is_patch_increment(commit_message));
 }
+
+#[rstest(
+    commit_message,
+    case("fix(  ): __proto__ will now be replaced with ___proto___ in parse (#1591)"),
+    case("fix(): fix enumeration for normalized path arguments (#1567)\n\n")
+)]
+fn test_is_patch_increment_empty_scope(commit_message: &str) {
+    assert_eq!(true, is_patch_increment(commit_message));
+}
+
+#[rstest(
+    commit_message,
+    case("feat()!: pull in yargs-parser@17.0.0 (#1553)\n\n"),
+    case("chore(\t): update dependency which to v2\n\n"),
+    case("docs(  ): state limitations of using command handlers returning promises\n\n")
+)]
+fn test_is_not_patch_increment_empty_scope(commit_message: &str) {
+    assert_eq!(false, is_patch_increment(commit_message));
+}
