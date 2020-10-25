ec5bd19677d019023cba360179872ea11fd5aa26
C <DeveloperC@protonmail.com>
2020-10-25 14:57:34 +0000

    test: adding major body generated tests

--- src/increment/tests/generated_tests/mod.rs
+++ src/increment/tests/generated_tests/mod.rs
@@ -4,18 +4,42 @@ mod utilities;
 
 #[test]
 fn test_generate_major_commits() {
-    let number_of_variants = 5;
+    let number_of_variants = 6;
     let upper_bound = 2_u32.pow(number_of_variants) as u32;
 
     for i in 1..upper_bound {
         //Given
-        let binary_string = format!("{:05b}", i); // Update when number_of_variants changes.
+        let binary_string = format!("{:06b}", i); // Update when number_of_variants changes.
         let commits = utilities::generate_major_commits(
             utilities::is_position_in_binary_string_true(&binary_string, 0),
             utilities::is_position_in_binary_string_true(&binary_string, 1),
             utilities::is_position_in_binary_string_true(&binary_string, 2),
             utilities::is_position_in_binary_string_true(&binary_string, 3),
             utilities::is_position_in_binary_string_true(&binary_string, 4),
+            utilities::is_position_in_binary_string_true(&binary_string, 5),
+        );
+
+        for commit_message in commits {
+            //When/Then
+            println!("{}", commit_message);
+            assert_eq!(true, is_major_increment(&*commit_message));
+        }
+    }
+}
+
+#[test]
+fn test_generate_major_body_commits() {
+    let number_of_variants = 4;
+    let upper_bound = 2_u32.pow(number_of_variants) as u32;
+
+    for i in 1..upper_bound {
+        //Given
+        let binary_string = format!("{:04b}", i); // Update when number_of_variants changes.
+        let commits = utilities::generate_major_body_commits(
+            utilities::is_position_in_binary_string_true(&binary_string, 0),
+            utilities::is_position_in_binary_string_true(&binary_string, 1),
+            utilities::is_position_in_binary_string_true(&binary_string, 2),
+            utilities::is_position_in_binary_string_true(&binary_string, 3),
         );
 
         for commit_message in commits {
@@ -28,18 +52,19 @@ fn test_generate_major_commits() {
 
 #[test]
 fn test_generate_minor_commits() {
-    let number_of_variants = 5;
+    let number_of_variants = 6;
     let upper_bound = 2_u32.pow(number_of_variants) as u32;
 
     for i in 1..upper_bound {
         //Given
-        let binary_string = format!("{:05b}", i); // Update when number_of_variants changes.
+        let binary_string = format!("{:06b}", i); // Update when number_of_variants changes.
         let commits = utilities::generate_minor_commits(
             utilities::is_position_in_binary_string_true(&binary_string, 0),
             utilities::is_position_in_binary_string_true(&binary_string, 1),
             utilities::is_position_in_binary_string_true(&binary_string, 2),
             utilities::is_position_in_binary_string_true(&binary_string, 3),
             utilities::is_position_in_binary_string_true(&binary_string, 4),
+            utilities::is_position_in_binary_string_true(&binary_string, 5),
         );
 
         for commit_message in commits {
@@ -52,18 +77,19 @@ fn test_generate_minor_commits() {
 
 #[test]
 fn test_generate_patch_commits() {
-    let number_of_variants = 5;
+    let number_of_variants = 6;
     let upper_bound = 2_u32.pow(number_of_variants) as u32;
 
     for i in 1..upper_bound {
         //Given
-        let binary_string = format!("{:05b}", i); // Update when number_of_variants changes.
+        let binary_string = format!("{:06b}", i); // Update when number_of_variants changes.
         let commits = utilities::generate_patch_commits(
             utilities::is_position_in_binary_string_true(&binary_string, 0),
             utilities::is_position_in_binary_string_true(&binary_string, 1),
             utilities::is_position_in_binary_string_true(&binary_string, 2),
             utilities::is_position_in_binary_string_true(&binary_string, 3),
             utilities::is_position_in_binary_string_true(&binary_string, 4),
+            utilities::is_position_in_binary_string_true(&binary_string, 5),
         );
 
         for commit_message in commits {
--- src/increment/tests/major/mod.rs
+++ src/increment/tests/major/mod.rs
@@ -32,6 +32,7 @@ fn test_is_major_increment_invalid_scope(commit_message: &str) {
     commit_message,
     case("fix(default): Remove undocumented alias of default() (#469)\n\nBREAKING CHANGE:\r\n\r\nremoved undocumented `defaults` alias for `default`.\r\n\r\n"),
     case("feat: apply default builder to command() and apply fail() handlers globally (#583)\n\nBREAKING CHANGE: fail is now applied globally.\r\nBREAKING CHANGE: we now default to an empty builder function when command is executed with no builder."),
+    case("feat: tweaks to API surface based on user feedback (#1726)\n\nBREAKING CHANGE: tweaks to ESM/Deno API surface: now exports yargs function by default; getProcessArgvWithoutBin becomes hidBin; types now exported for Deno."),
 )]
 fn test_is_major_increment_footer(commit_message: &str) {
     assert_eq!(true, is_major_increment(commit_message));
