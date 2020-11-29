e45cb8f529bdaa197e6a99a59a895333a0614b55
C <DeveloperC@protonmail.com>
2020-11-29 16:37:45 +0000

    fix: tightening major increment type regex

--- src/increment/regex/mod.rs
+++ src/increment/regex/mod.rs
@@ -1,15 +1,16 @@
 use regex::Regex;
 
 const OPTIONAL_PRECEDING_WHITESPACE: &str = "^([[:space:]])*";
-const ANY_REGEX: &str = "([[:digit:]]|[[:alpha:]]|_|-|[[:space:]])*";
+const ANY_CHARACTER_REGEX: &str = "([[:digit:]]|[[:alpha:]])+";
+const OPTIONAL_ANY_REGEX: &str = "([[:digit:]]|[[:alpha:]]|_|-|[[:space:]])*";
 
 lazy_static! {
-    static ref OPTIONAL_SCOPE_REGEX: String = format!(r"(\({}\))?", ANY_REGEX);
+    static ref OPTIONAL_SCOPE_REGEX: String = format!(r"(\({}\))?", OPTIONAL_ANY_REGEX);
     pub(super) static ref MAJOR_TITLE_INCREMENT_REGEX: Regex = Regex::new(
         format!(
             r"(?i){}({})(!{}|{}!):",
             &*OPTIONAL_PRECEDING_WHITESPACE,
-            &*ANY_REGEX,
+            &*ANY_CHARACTER_REGEX,
             &*OPTIONAL_SCOPE_REGEX,
             &*OPTIONAL_SCOPE_REGEX
         )
