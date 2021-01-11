6a6d11e098a5276355e270a241d4ea7c39df716d
C <DeveloperC@protonmail.com>
2021-01-11 18:27:36 +0000

    feat: ignore v prefix semantic version

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,4 +1,4 @@
-use semver::Version;
+use semver::{SemVerError, Version};
 use structopt::{clap::ArgGroup, StructOpt};
 
 #[derive(Debug, StructOpt)]
@@ -24,6 +24,7 @@ pub struct Arguments {
 
     #[structopt(
         long,
+        parse(try_from_str = parse_version),
         help = "The initial Semantic Versioning to begin calculations from."
     )]
     pub from_version: Version,
@@ -36,6 +37,7 @@ pub struct Arguments {
 
     #[structopt(
         long,
+        parse(try_from_str = parse_version),
         help = "The `--current-version` Semantic Versioning is asserted to be equal or larger than the calculated Semantic Versioning. The calculated Semantic Versioning is not printed to standard out, if the assertion is not met then it exits with a non zero exit code."
     )]
     pub current_version: Option<Version>,
@@ -45,4 +47,12 @@ pub struct Arguments {
         help = "Monorepo usage is supported, the version is calculated only from commits altering the specified directory."
     )]
     pub monorepo: Option<String>,
+}
+
+fn parse_version(src: &str) -> Result<Version, SemVerError> {
+    if src.starts_with('v') || src.starts_with('V') {
+        return Version::parse(&src[1..]);
+    }
+
+    Version::parse(src)
 }
