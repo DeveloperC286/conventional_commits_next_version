5b09dd2d79cdec71c9160e7113cfa3145e1f6d50
DeveloperC <DeveloperC@protonmail.com>
2021-06-09 21:08:06 +0100

    test: improved test assertion messages

--- src/model/commits/commit/tests/mod.rs
+++ src/model/commits/commit/tests/mod.rs
@@ -7,19 +7,6 @@ mod major;
 mod minor;
 mod patch;
 
-fn is_major_footer_increment(commit_message: &str) {
-    let commit = Commit {
-        message: commit_message.to_string(),
-    };
-
-    assert_eq!(
-        true,
-        commit.is_major_footer_increment(),
-        "{:?} is not is_major_footer_increment()",
-        commit_message
-    );
-}
-
 fn is_major_title_increment(commit_message: &str) {
     let commit = Commit {
         message: commit_message.to_string(),
@@ -28,7 +15,7 @@ fn is_major_title_increment(commit_message: &str) {
     assert_eq!(
         true,
         commit.is_major_title_increment(),
-        "{:?} is not is_major_title_increment()",
+        "{:?} does not match a major title Semantic Versioning increment commit message.",
         commit_message
     );
 }
@@ -41,7 +28,20 @@ fn is_not_major_title_increment(commit_message: &str) {
     assert_eq!(
         false,
         commit.is_major_title_increment(),
-        "{:?} is_major_title_increment()",
+        "{:?} matches a major title Semantic Versioning increment commit message.",
+        commit_message
+    );
+}
+
+fn is_major_footer_increment(commit_message: &str) {
+    let commit = Commit {
+        message: commit_message.to_string(),
+    };
+
+    assert_eq!(
+        true,
+        commit.is_major_footer_increment(),
+        "{:?} does not match a major footer Semantic Versioning increment commit message.",
         commit_message
     );
 }
@@ -54,7 +54,7 @@ fn is_minor_increment(commit_message: &str) {
     assert_eq!(
         true,
         commit.is_minor_increment(),
-        "{:?} is not is_minor_increment()",
+        "{:?} does not match a minor Semantic Versioning increment commit message.",
         commit_message
     );
 }
@@ -67,7 +67,7 @@ fn is_not_minor_increment(commit_message: &str) {
     assert_eq!(
         false,
         commit.is_minor_increment(),
-        "{:?} is not is_minor_increment()",
+        "{:?} matches a minor Semantic Versioning increment commit message.",
         commit_message
     );
 }
@@ -80,7 +80,7 @@ fn is_patch_increment(commit_message: &str) {
     assert_eq!(
         true,
         commit.is_patch_increment(),
-        "{:?} is not is_patch_increment()",
+        "{:?} does not match a patch Semantic Versioning increment commit message.",
         commit_message
     );
 }
@@ -93,7 +93,7 @@ fn is_not_patch_increment(commit_message: &str) {
     assert_eq!(
         false,
         commit.is_patch_increment(),
-        "{:?} is_patch_increment()",
+        "{:?} matches a patch Semantic Versioning increment commit message.",
         commit_message
     );
 }
