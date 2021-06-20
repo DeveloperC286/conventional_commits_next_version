3f1f89cff381e28b6339dff0c933435653d4b6f0
DeveloperC <DeveloperC@protonmail.com>
2021-06-20 20:49:13 +0100

    test: adding monorepo unit tests

--- src/model/monorepo/tests/mod.rs
+++ src/model/monorepo/tests/mod.rs
@@ -1,1 +1,33 @@
+use super::*;
 
+#[test]
+fn test_files_within_monorepo() {
+    // Given
+    let monorepo = Monorepo {
+        monorepo: Some("lib/".to_string()),
+    };
+    let files: HashSet<String> = {
+        let mut files: HashSet<String> = HashSet::new();
+        files.insert("lib/src/lib.rs".to_string());
+        files
+    };
+
+    // When/Then
+    assert!(monorepo.is_files_within(files));
+}
+
+#[test]
+fn test_files_not_within_monorepo() {
+    // Given
+    let monorepo = Monorepo {
+        monorepo: Some("lib/".to_string()),
+    };
+    let files: HashSet<String> = {
+        let mut files: HashSet<String> = HashSet::new();
+        files.insert("bin/src/main.rs".to_string());
+        files
+    };
+
+    // When/Then
+    assert!(!monorepo.is_files_within(files));
+}
