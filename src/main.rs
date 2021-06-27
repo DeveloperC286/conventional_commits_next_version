ab501538858a5433e06ceb691116d34cdc672cbe
DeveloperC <DeveloperC@protonmail.com>
2021-06-27 17:25:36 +0100

    refactor: adding version printing for debugging

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ const ERROR_EXIT_CODE: i32 = 1;
 
 fn main() {
     pretty_env_logger::init();
+    trace!("Version {}.", env!("CARGO_PKG_VERSION"));
     let arguments = cli::Arguments::from_args();
     trace!("The command line arguments provided are {:?}.", arguments);
 
