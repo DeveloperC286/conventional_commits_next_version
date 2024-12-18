5771b77bd9d729ff4ba5b421d8d9f8090eae35f7
DeveloperC <DeveloperC@protonmail.com>
2024-12-18 10:59:33 +0000

    build: adding cargo --locked and removing --workspace

--- ci/unit-test.sh
+++ ci/unit-test.sh
@@ -3,6 +3,4 @@
 set -o errexit
 set -o xtrace
 
-cargo test --verbose --workspace
-cargo test --verbose --workspace --all-features
-cargo test --verbose --workspace --no-default-features
+cargo test --verbose --locked
